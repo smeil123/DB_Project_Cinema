234d3dcf21ef1188907e1d1970c64bdaf6d8a1fa
yuuun <ynkim31@naver.com>
2018-05-02 19:58:23 +0900

    Ticketing_Calendar_Show

--- WebApplication2/WebApplication2/Ticketing.aspx.designer.cs
+++ WebApplication2/WebApplication2/Ticketing.aspx.designer.cs
@@ -56,5 +56,23 @@ namespace WebApplication2 {
         /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
         /// </remarks>
         protected global::System.Web.UI.WebControls.Label Label4;
+        
+        /// <summary>
+        /// Calendar1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Calendar Calendar1;
+        
+        /// <summary>
+        /// CheckBoxList1 컨트롤입니다.
+        /// </summary>
+        /// <remarks>
+        /// 자동 생성 필드입니다.
+        /// 수정하려면 디자이너 파일에서 코드 숨김 파일로 필드 선언을 이동하세요.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CheckBoxList CheckBoxList1;
     }
 }
